0489677b8cb6a96ad6e2ca35c92b568a323c8d2f
lzkelley <lkelley@cfa.harvard.edu>
2015-09-06 11:08:02 -0700

    Print basic count stats after running.

--- Feeder.py
+++ Feeder.py
@@ -5,19 +5,41 @@
 
 import Sources
 import rss
+from datetime import datetime
+import numpy as np
 
 
 def main():
+    
+    beg = datetime.now()
+
     sources = Sources.sources
     names, feeds = loadSources(sources)
 
+    numFeeds = 0
+    numStories = 0
+
     for nn,ff in zip(names, feeds):
         titles = rss.stringTitles(ff, True)
+        if( np.size(titles) > 0 ):
+            numFeeds += 1
+            numStories += np.size(titles)
+
         print nn
         for tt in titles:
             print "\t",tt
 
         print ""
+
+
+    
+        
+    print "Feeds: %d" % (numFeeds)
+    print "Stores: %d" % (numStories)
+
+    end = datetime.now()
+    print "After %s" % (str(end-beg))
+
     
     return
 
