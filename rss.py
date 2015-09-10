859b11a930fc603c2de99a0cb87fad42174d2e26
lzkelley <lkelley@cfa.harvard.edu>
2015-09-10 14:46:45 -0700

    Created classes 'Feed' and 'Article' to handle/parse rss feeds and entries respectively.  Working.

--- rss.py
+++ rss.py
@@ -2,10 +2,119 @@
 
 """
 
-import feedparser
+import feedparser, time
+from bs4 import BeautifulSoup
 import numpy as np
 
+import MyLogger, Settings
+
 TITLE_WIDTH = 140
+
+
+
+
+class Article(object):
+    
+    def __init__(self, ent):
+        self._ent = ent
+        self.valid = False
+
+        try:
+            self.published = ent.published_parsed
+            self.title = ent.title
+            self.link = ent.link
+            self.updated = ent.updated_parsed
+            self.summary = BeautifulSoup(ent.summary).text
+        except:
+            self.published = None
+            self.title = None
+            self.link = None
+            self.updated = None
+            self.summary = None
+            return
+
+        self.valid = True
+        return
+
+    def _hasGet(self, ent, key):
+        if( hasattr(ent, key) ): return getattr(ent, key)
+        else: return None
+
+
+
+class Feed(object):
+    
+    def __init__(self, url):
+        """
+        """
+        rssf = feedparser.parse(url)
+        self._rssfeed = rssf
+        self.status = rssf.status
+        self.url = url
+        self.valid = False
+        self.articles = []
+        self.count = 0
+
+        if( self.status != 200 or not hasattr(rssf, 'feed') ): 
+            print "\tBad status/feed!"
+            return
+
+        if( not hasattr(rssf.feed, 'title') ):
+            print "\tBad title!"
+            return
+
+        self.title = rssf.feed.title
+
+        self.time = self._hasGet(rssf, 'updated_parsed')
+        if( self.time is None ): self.time = self._hasGet(rssf, 'published_parsed')
+
+        if( hasattr(rssf, 'entries') ):
+            for ent in rssf.entries:
+                art = Article(ent)
+                if( art.valid ): self.articles.append(art)
+
+            self.count = len(self.articles)
+
+        if( self.count == 0 ): return
+
+        self.valid = True
+        return
+
+
+    def _hasGet(self, rssf, key):
+        """
+        Check for attribute ``key`` in given rssfeed or rssfeed.feed.  Return ``None`` if neither.
+        """
+
+        if( hasattr(rssf, key) ):
+            return getattr(rssf, key)
+        elif( hasattr(rssf.feed, key) ):
+            return getattr(rssf.feed, key)
+
+        return None
+    
+
+
+
+def getFeeds(sourceList, log):
+
+    
+    log.debug("getFeeds()")
+
+    ids = []
+    feeds = []
+    for ii, src in enumerate(sourceList.sources):
+        feed = Feed(src.url)
+        print "%d, %s --- %d" % (ii, src.name, feed.count)
+        if( feed.valid ): 
+            ids.append(ii)
+            feeds.append(feed)
+
+
+    return ids, feeds
+    
+
+
 
 
 def entries(addy):
