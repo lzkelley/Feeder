3ab71711e85d318d18530d200e55dbd3015c0ec0
lzkelley <lkelley@cfa.harvard.edu>
2015-09-13 14:03:07 -0700

    Added routine '_getFilename' to construct a valid filename for each Source.  Added docstrings.  Moved global constants to 'Settings.py' file.

--- Source.py
+++ Source.py
@@ -10,9 +10,6 @@ import numpy as np
 
 import MyLogger, Settings
 
-WIDTH_TITLE = 100
-WIDTH_TIME  = 30
-WIDTH_NAME  = 50
 
 class Article(object):
     
@@ -42,7 +39,8 @@ class Article(object):
         Note: DO NOT OVERRIDE `__str__` returning unicode!
         """
         myStr = u"{0:{w0}.{w0}} - {1:{w1}.{w1}}"
-        myStr = myStr.format(self.title, time.asctime(self.updated), w0=WIDTH_TITLE, w1=WIDTH_TIME)
+        myStr = myStr.format(self.title, time.asctime(self.updated), 
+                             w0=Settings.STR_TITLE_LEN, w1=Settings.STR_TIME_LEN)
         return myStr
 
 
@@ -54,11 +52,31 @@ class Article(object):
 
 
 class Source(object):
+    """
+    Store data for individual feed source, and manage loading and storing feed articles.
 
-    def __init__(self, url, name='', subname=''):
+    Methods
+    -------
+        str          : Construct a string description of this object using its title and time.
+        getFeed      : Load the RSS feed from stored url.
+        _strTimes    : Store string representations of different time attributes.
+        _mostRecent  : Find time of the most recent article.
+        _hasGet      : Check for attribute, retrieve if possible (otherwise ``None``).
+        _getFilename : Construct a viable filename for this ``Source``.
+
+    """
+
+
+    def __init__(self, url, name='', subname='', filename='', filetime=None):
+        ## Parameters Loaded from SourceList files
         self.url = url
         self.name = name
         self.subname = subname
+
+        ## Set filename, construct if needed
+        if( len(filename) > 0 ): self._filename = filename
+        else:                    self._filename = self._getFilename()
+        self.file_time = filetime
 
         self.valid = False
         self.articles = []
@@ -69,16 +87,19 @@ class Source(object):
         self.time = None
         self.feed_time = None
         self.article_time = None
-
         self.time_str = ''
         self.feed_time_str = ''
         self.article_time_str = ''
+        self.file_time_str = ''
 
         return
+      
 
     
     def str(self):
         """
+        Construct a string description of this object using its title and time.
+
         Note: DO NOT OVERRIDE `__str__` returning unicode!
         """
         myStr = u"{0} - {1}"
@@ -88,23 +109,35 @@ class Source(object):
 
     def getFeed(self):
         """
+        Load the RSS feed from this ``Source``'s stored url.
+
+        Uses ``feedparser`` to parse the RSS feed, converting a list of 'entries' into a list of
+        ``Article`` objects stored to the ``.articles`` variable.  Time information is updated
+        based on the metadata given in the RSS feed, and the timestamps of the articles themselves.
+
+        Returns
+        -------
+            retval <bool> : `True` on success, `False` otherwise
+
         """
         
+        # Get Parsed Feed
         feed = feedparser.parse(self.url)
         self._feed = feed
         self.status = feed.status
 
-        if( self.status != 200 or not hasattr(feed, 'feed') ): 
-            return self.valid
+        # Check if source feed seems valid
+        self.valid = False # Make sure this is False by default
+        if( self.status != 200 or not hasattr(feed, 'feed') or not hasattr(feed.feed, 'title') ): 
+            return False
 
-        if( not hasattr(feed.feed, 'title') ):
-            return self.valid
-
+        # Set basic parameters (if available)
         self.title = feed.feed.title
-
+        #     Look for something describing time this feed was updated
         self.feed_time = self._hasGet(feed, 'updated_parsed')
         if( self.feed_time is None ): self.feed_time = self._hasGet(feed, 'published_parsed')
 
+        # Look for 'entries', use them to create ``Articles``
         if( hasattr(feed, 'entries') ):
             for ent in feed.entries:
                 art = Article(ent)
@@ -112,27 +145,38 @@ class Source(object):
 
             self.count = len(self.articles)
 
-        if( self.count == 0 ): return self.valid
+        # If no valid articls found, return False
+        if( self.count == 0 ): return False
 
+        # Find most recent time from articles
         self.article_time = self._mostRecent()
 
-        if( self.feed_time is not None ):
-            self.time = self.feed_time
-        else:
-            self.time = self.article_time
+        # Set ``.time`` as the most reliable measure of updated time
+        if( self.feed_time is not None ): self.time = self.feed_time
+        else:                             self.time = self.article_time
 
-
-        self.time_str = time.asctime(self.time) #.encode('utf-8')
-        if( self.feed_time is not None ): self.feed_time_str = time.asctime(self.feed_time)
-        if( self.article_time is not None ): self.article_time_str = time.asctime(self.article_time)
+        # Update string times
+        self._strTimes()
 
         self.valid = True
-        return self.valid
+        return True
+
+
+    
+    def _strTimes(self):
+        """
+        Store string representations of different time attributes.
+        """
+        self.time_str = time.asctime(self.time)
+        if( self.feed_time is not None ): self.feed_time_str = time.asctime(self.feed_time)
+        if( self.article_time is not None ): self.article_time_str = time.asctime(self.article_time)
+        if( self.file_time is not None ): self.file_time_str = time.asctime(self.file_time)
+        return
 
 
     def _mostRecent(self):
         """
-        Find time of most recent article.
+        Find time of the most recent article.
         """
         times = [ art.updated for art in self.articles ]
         if( len(times) == 0 ):
@@ -161,26 +205,30 @@ class Source(object):
         return None
     
 
-'''
-def stringTitles(entries, times=False, width=TITLE_WIDTH):
-    titles = [ ent.title.encode('utf-8') for ent in entries ]
-    titles = [ ent.title for ent in entries ]
+    def _getFilename(self):
+        """
+        Construct a viable filename for this ``Source``.
 
-    strings = []
+        Notes:
+            `name`_`subname`[__`url`]
+            If the combination of ``name`` and ``subname`` are not long enough (compared to 
+            ``Settings.FILENAME_MIN_LEN``), then a trimmed version of the URL is appended.
+            The only preserved characters are alpha-numeric and underscore ('_').
 
-    if( times ):
-        times = [ ent.published_parsed for ent in entries ]
-        times = _formatDatetime(times, date=True, time=True)
-        for tit,tim in zip(titles,times):
-            strings.append(u"{:<{wid}.{wid}s}   {:21.21s}".format(tit,tim,wid=width))
+        Returns
+        -------
+            retval <str> : filename
 
-    else:
-        
-        for tit in titles:
-            strings.append(u"{:>140.140s}".format(tit))
+        """
 
+        # Construct candidate filename from ``name`` and ``subname``
+        fname = self.name.strip() + '_' + self.subname.strip()
+        # If candidate is not long enough, append cleaned-``url`` string
+        if( len(fname) <= Settings.FILENAME_MIN_LEN ): 
+            urlStr = self.url.strip().replace('http://','').replace('www.','').split('/')[0]
+            fname += '__' + urlStr
 
-    return strings
+        # Remove everything but underscore ('_') and alphanumeric characters
+        fname = ''.join(ch for ch in fname if (ch.isalnum() or ch == '_') )
 
-# stringTitles()
-'''
+        return fname
