dcd39534354fada96034006a6c971d4521a06020
lzkelley <lkelley@cfa.harvard.edu>
2015-09-13 18:06:16 -0700

    Added functions 'loadArticles()' and 'saveArticles()' to load and save 'Article' objects as dictionaries in a json file.
    This uses the methods 'ent_to_dict()' and 'dict_to_ent()' which convert 'feed.entries' dictionary objects containing time.struct_time objects into purely string based dictionaries which can be saved.  Currently, before saving, the title of articles are checked against those already in the save file --- to see if they are new.  Something more robust needs to happen.  Also, the save file is a list of dictionaries.  It would probably be better to just have a dictionary per line --- so that new entries can easily be appended (if we know they are new, based on e.g. datetime information).

--- Source.py
+++ Source.py
@@ -1,10 +1,22 @@
 """
 
+Objects
+-------
+    Article
+    Source
+
+
+Methods
+-------
+    ent_to_dict : 
+    dict_to_ent : 
+
+
 """
 
 from __future__ import unicode_literals
 
-import feedparser, time
+import feedparser, time, json, os
 from bs4 import BeautifulSoup
 import numpy as np
 
@@ -17,29 +29,49 @@ class Article(object):
         self._ent = ent
         self.valid = False
 
+        self.time = None
+        self.time_updated = None
+        self.time_published = None
+        self.time_updated_str = ''
+        self.time_published_str = ''
+        self.time_str = ''
+
         try:
-            self.published = ent.published_parsed
-            self.title = ent.title
-            self.link = ent.link
-            self.updated = ent.updated_parsed
-            self.summary = BeautifulSoup(ent.summary).text
+            self.title = ent['title']
+            self.link = ent['link']
+            self.summary = BeautifulSoup(ent['summary']).text
+
+            if( ent.has_key('updated_parsed') ): self.time_updated = ent['updated_parsed']
+            if( ent.has_key('published_parsed') ): self.time_published = ent['published_parsed']
+
         except:
-            self.published = None
             self.title = None
             self.link = None
-            self.updated = None
             self.summary = None
             return
 
+
+        if( self.time_updated is not None ): 
+            self.time_updated_str = time.asctime(self.time_updated)
+
+        if( self.time_published is not None ): 
+            self.time_published_str = time.asctime(self.time_published)
+
+        if( self.time_updated is not None ): self.time = self.time_updated
+        elif( self.time_published is not None ): self.time = self.time_published
+
+        if( self.time is not None ): self.time_str = time.asctime(self.time)
+
         self.valid = True
         return
+
 
     def str(self):
         """
         Note: DO NOT OVERRIDE `__str__` returning unicode!
         """
         myStr = u"{0:{w0}.{w0}} - {1:{w1}.{w1}}"
-        myStr = myStr.format(self.title, time.asctime(self.updated), 
+        myStr = myStr.format(self.title, self.time_str, 
                              w0=Settings.STR_TITLE_LEN, w1=Settings.STR_TIME_LEN)
         return myStr
 
@@ -162,6 +194,90 @@ class Source(object):
         return True
 
 
+
+    def loadArticles(self, fname=None):
+        """
+        """
+
+        print "\nLOAD"
+
+        if( fname is None ): fname = self._filename
+
+        if( not os.path.exists(fname) ): 
+            estr = "ERROR: file '%s' does not exist!" % (fname)
+            print estr
+            return []
+
+        try:
+            data = json.load( open(fname, 'r') )
+            print "Loaded data from '%s'" % (fname)
+        except:
+            estr = "ERROR: could not load json from '%s'" % (fname)
+            print estr
+            return []
+
+        ents = [ dict_to_ent(dic) for dic in data ]
+        arts = [ Article(ee) for ee in ents ]
+        print "Loaded %d articles" % (len(arts))
+
+        return arts
+
+
+
+    def saveArticles(self, fname=None):
+        """
+        """
+
+        print "\nSAVE"
+
+        if( fname is None ): fname = self._filename
+
+        print "Filename = ", fname
+        
+        if( len(fname) == 0 ):
+            estr = "Error: invalid filename '%s'" % (fname)
+            print estr
+            return False
+
+        
+        arts = []
+        if( os.path.exists(fname) ):
+            print "Path '%s' exists" % (fname)
+            arts += self.loadArticles(fname)
+            print "\tLoaded %d articles" % (len(arts))
+
+        else:
+            print "Path '%s' does not exist" % (fname)
+        
+
+        titles = [ aa.title for aa in arts ]
+        newArts = 0
+        for aa in self.articles:
+            if( aa.title not in titles ): 
+                arts.append(aa)
+                newArts += 1
+
+        print "Adding %d new articles" % (newArts)
+
+        ## Convert articles to string dictionaries
+        print "Converting entries to dictionaries"
+        data = [ ent_to_dict(aa._ent) for aa in arts ]
+        
+        print "Saving to '%s'" % (fname)
+        json.dump(data, open(fname, 'w'))
+
+        if( not os.path.exists(fname) ):
+            estr = "ERROR: did not save to '%s'" % (fname)
+            print estr
+            return False
+
+
+        print "Saved"
+        self.saved = True
+
+        return True
+
+
     
     def _strTimes(self):
         """
@@ -178,7 +294,7 @@ class Source(object):
         """
         Find time of the most recent article.
         """
-        times = [ art.updated for art in self.articles ]
+        times = [ art.time for art in self.articles if art.time is not None ]
         if( len(times) == 0 ):
             print "WARNING: NO TIMES!  %s" % (self.url)
         elif( len(times) == 1 ):
@@ -231,4 +347,44 @@ class Source(object):
         # Remove everything but underscore ('_') and alphanumeric characters
         fname = ''.join(ch for ch in fname if (ch.isalnum() or ch == '_') )
 
+        fname = Settings.Settings().dir_data + fname + Settings.SOURCE_FILE_TYPE
+
         return fname
+
+
+
+def ent_to_dict(ent):
+    """
+    """
+
+    # Copy data to new dictionary
+    dic = dict(ent)
+
+    # Convert ``struct_time`` objects to strings
+    for key in dic.keys():
+        if( key.endswith('_parsed') ):
+            struct_time = dic[key]
+            strTime = "{:.3f}".format(time.mktime( dic[key] ))
+            dic[key] = strTime
+
+    
+    return dic
+
+
+
+def dict_to_ent(dic):
+    """
+    """
+
+    # Copy data to new dictionary
+    ent = dict(dic)
+
+    # String times to ``struct_time``
+    for key in ent.keys():
+        if( key.endswith('_parsed') ):
+            fltTime = np.float(ent[key])
+            struct_time = time.localtime(fltTime)
+            ent[key] = struct_time
+
+    return ent
+    
